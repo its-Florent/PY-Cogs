7a6f2a98ce9df0e8b5f056d5ba9bcb7fe773e5b6
its_florent <70605487+itsflorent@users.noreply.github.com>
2021-08-18 16:49:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='.') #intents=discord.Intents.all()) # Optional due to the Discord intents 
 
 
-extensions = ['test']# Your Cog
+extensions = ['test']# Your extensions (Path + filename!)
+
 
 if __name__ == '__main__':
     for extension in extensions:
         try:
-            bot.load_extension(extension)# Cog Load
+            bot.load_extension(extension)# Load all extensions from the list.
         except Exception as error:
-            print('{} konnte nicht geladen werden. [{}]'.format(extension, error))
+            print('{} could not be loaded. [{}]'.format(extension, error))
 
 
-@bot.event  # Ready Event
+@bot.event
 async def on_ready():
-    print("Bot Online mit:")
+    print("Bot online with:")
     print("Username: ", bot.user.name)
     print("User ID: ", bot.user.id)
 
 
-bot.run("TOKEN") # Your Bot Token from https://discord.com/developers/applications
+bot.run("TOKEN") # Your Token from discord.com/developers
